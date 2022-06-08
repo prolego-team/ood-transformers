ae4a8012fe36751e708f054c96e8494a4f280c2e
Alex Cunliffe <alex@prolego.io>
2022-06-08 15:25:47 -0500

    update requirements for compatibility with cookiecutter

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
         "ood_transformers"
     ],
     install_requires=[
-        "click==8.0.3",
-        "matplotlib==3.4.3",
-        "nltk==3.6.5",
+        "click>=8.0.3",
+        "matplotlib>=3.4.3",
+        "nltk>=3.6.5",
         "transformers==4.9.1",
         "torch==1.9.0",
-        "numpy==1.21.1",
+        "numpy>=1.21.1",
         "text-classification @ git+https://git@github.com/prolego-team/text-classification@v0.2#egg=text-classification"
     ]
 )
