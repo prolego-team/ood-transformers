46e83a77fc691918e6306fbea5d907ae39255149
Alex Cunliffe <alex@prolego.io>
2022-06-08 15:28:19 -0500

    another package dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "matplotlib>=3.4.3",
         "nltk>=3.6.5",
         "transformers==4.9.1",
-        "torch==1.9.0",
+        "torch>=1.9.0",
         "numpy>=1.21.1",
         "text-classification @ git+https://git@github.com/prolego-team/text-classification@v0.2#egg=text-classification"
     ]
